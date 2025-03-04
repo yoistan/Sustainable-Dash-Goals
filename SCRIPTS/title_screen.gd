33b92f448e46ee89996dc7486d1eb4bc54edf29f
pi <sophiaatienza998@gmail.com>
2025-03-04 16:27:38 +0800

    added font and made title screen

---
+++ SCRIPTS/title_screen.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _on_play_button_pressed():
+	pass # play first scene
+
+func _on_settings_button_pressed():
+	pass # insert here settings screen scene
+
+func _on_quit_button_pressed():
+	get_tree().quit()
