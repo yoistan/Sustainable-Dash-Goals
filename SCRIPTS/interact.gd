45aaf6eb6ac6b8ce67d1339306ce12950f5bd2ff
pi <sophiaatienza998@gmail.com>
2025-03-13 09:30:29 +0800

    connected the scenes with tape (madaming bugs)
    need to establish a DialogueManager or a dialogue system and CutsceneManager
    
    temporary fix lang for presentation

--- SCRIPTS/interact.gd
+++ SCRIPTS/interact.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+@onready var sprite = $Sprite2D
+var lauraCutscene = load("res://SCENES/MINIGAMES/MiniGame1/game_1_dia.tscn")
+
+func _ready():
+	$InteractionArea.interact = Callable(self, "_on_interact")
+
+func _on_interact():
+	print("interacted")
+	#get_parent().change_scene_to_file("res://SCENES/MINIGAMES/MiniGame1/game_1_dia.tscn")
